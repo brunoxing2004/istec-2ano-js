4235576a9a888bd50b5e0381729a3f9f2ce7881b
brunoxing2004 <mail@brunosilva.co.uk>
2024-01-15 22:22:02 +0000

    Exercício 4

--- script.js
+++ script.js
@@ -3,8 +3,18 @@ function attachBuyEvents() {
 
   for (const button of buttons) {
     button.addEventListener('click', function (e) {
-      console.log('BUY!');
-      console.log(e.currentTarget);
+      const parentArticle = e.currentTarget.parentElement;
+      parentArticle.classList.toggle('sale');
+
+      const productId = parentArticle.getAttribute('data-id');
+      const productName = parentArticle.querySelector('.product-name').textContent;
+      const productPrice = parentArticle.querySelector('.product-price').textContent;
+      const quantity = parentArticle.querySelector('.quantity').value;
+
+      console.log('ID:', productId);
+      console.log('Name:', productName);
+      console.log('Price:', productPrice);
+      console.log('Quantity:', quantity);
     });
   }
 }
