b50559cd91fcb1cc691fa1d05fdc0965822f5f00
brunoxing2004 <mail@brunosilva.co.uk>
2024-01-15 22:18:59 +0000

    Exercício2

--- script.js
+++ script.js
@@ -1,23 +1,9 @@
-function attachBuyEvents() {
-    const buttons = document.querySelectorAll('#products button');
+function changeAllArticleColors() {
+  const articles = document.querySelectorAll('#products article');
   
-    for (const button of buttons) {
-      button.addEventListener('click', function (e) {
-        const parentArticle = e.currentTarget.parentElement;
-        parentArticle.classList.toggle('sale');
-        
-        const productId = parentArticle.getAttribute('data-id');
-        const productName = parentArticle.querySelector('.product-name').textContent;
-        const productPrice = parentArticle.querySelector('.product-price').textContent;
-        const quantity = parentArticle.querySelector('.quantity').value;
-  
-        console.log('ID:', productId);
-        console.log('Name:', productName);
-        console.log('Price:', productPrice);
-        console.log('Quantity:', quantity);
-      });
-    }
+  for (const article of articles) {
+    article.classList.add('sale');
   }
-  
-  attachBuyEvents();
-  
+}
+
+changeAllArticleColors();
